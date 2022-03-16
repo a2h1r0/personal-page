2791dc9370682ceaccda0a0b068d64952c87f708
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2022-03-16 20:53:32 +0900

    npm install --save-dev @prettier/plugin-pug

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-v-row(justify='center' align='center')
-  v-col(cols='12' sm='8' md='6')
+v-row(justify='center', align='center')
+  v-col(cols='12', sm='8', md='6')
     v-card.logo.py-4.d-flex.justify-center
       NuxtLogo
       VuetifyLogo
@@ -14,17 +14,31 @@ v-row(justify='center' align='center')
           | applications.
         p
           | For more information on Vuetify, check out the
-          a(href='https://vuetifyjs.com' target='_blank' rel='noopener noreferrer')
+          a(
+            href='https://vuetifyjs.com',
+            target='_blank',
+            rel='noopener noreferrer'
+          )
             | documentation
           | .
         p
           | If you have questions, please join the official
-          a(href='https://chat.vuetifyjs.com/' target='_blank' rel='noopener noreferrer' title='chat')
+          a(
+            href='https://chat.vuetifyjs.com/',
+            target='_blank',
+            rel='noopener noreferrer',
+            title='chat'
+          )
             | discord
           | .
         p
           | Find a bug? Report it on the github
-          a(href='https://github.com/vuetifyjs/vuetify/issues' target='_blank' rel='noopener noreferrer' title='contribute')
+          a(
+            href='https://github.com/vuetifyjs/vuetify/issues',
+            target='_blank',
+            rel='noopener noreferrer',
+            title='contribute'
+          )
             | issue board
           | .
         p
@@ -34,14 +48,22 @@ v-row(justify='center' align='center')
           em
             small — John Leider
         hr.my-3
-        a(href='https://nuxtjs.org/' target='_blank' rel='noopener noreferrer')
+        a(
+          href='https://nuxtjs.org/',
+          target='_blank',
+          rel='noopener noreferrer'
+        )
           | Nuxt Documentation
         br
-        a(href='https://github.com/nuxt/nuxt.js' target='_blank' rel='noopener noreferrer')
+        a(
+          href='https://github.com/nuxt/nuxt.js',
+          target='_blank',
+          rel='noopener noreferrer'
+        )
           | Nuxt GitHub
       v-card-actions
         v-spacer
-          v-btn(color='primary' nuxt='' to='/inspire')  Continue 
+          v-btn(color='primary', nuxt='', to='/inspire') Continue
 </template>
 
 <script>
