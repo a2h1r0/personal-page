b699463c34d287df5385ee66ec8676c55f11147e
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2022-05-03 17:57:50 +0900

    Journal Paper表示部分の作成

--- pages/publication.vue
+++ pages/publication.vue
@@ -1,12 +1,61 @@
 <template lang="pug">
 v-container
-  p 準備中
+  v-container
+    h2 学術論文誌発表論文 / Journal paper
+    v-list(color='dark')
+      v-list-item(v-for='publication in publications.journalPaper')
+        span(v-for='author in publication.authors') {{ author }}:&nbsp;
+        span {{ publication.title }},&nbsp;
+        span {{ publication.book }},&nbsp;
+        span(v-if='publication.volume') {{ publication.volume }},&nbsp;
+        span(v-if='publication.number') {{ publication.number }},&nbsp;
+        span(v-if='publication.issue') {{ publication.issue }},&nbsp;
+        span {{ publication.pageFrom }}&ndash;
+        span {{ publication.pageTo }},&nbsp;
+        span {{ publication.date }}
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+export type DataType = {
+  publications: {
+    journalPaper: [
+      {
+        authors: string[]
+        title: string
+        book: string
+        volume: Number | null
+        number: Number | null
+        issue: Number | null
+        pageFrom: Number
+        pageTo: Number
+        date: string
+      }
+    ]
+  }
+}
+
 export default Vue.extend({
+  data(): DataType {
+    return {
+      publications: {
+        journalPaper: [
+          {
+            authors: ['me'],
+            title: 'test',
+            book: 'book',
+            volume: 1,
+            number: 2,
+            issue: null,
+            pageFrom: 10,
+            pageTo: 100,
+            date: '2022-01-01',
+          },
+        ],
+      },
+    }
+  },
   head(): object {
     return {
       title: '業績',
