7e1944ad8a41ab18b6abf28bba80ca02f025a67e
a2h1r0 <53073064+a2h1r0@users.noreply.github.com>
2023-03-05 12:08:37 +0900

    carouselの設置

--- pages/contributions.vue
+++ pages/contributions.vue
@@ -4,6 +4,24 @@ v-container
     h2 hoge
     p hoge app
 
+    v-card.mx-auto(elevation='24', max-width='444')
+      v-carousel(cycle, height='300', hide-delimiter-background)
+        v-carousel-item
+          v-sheet(color='green', height='100%')
+            .d-flex.fill-height.justify-center.align-center
+              .text-h2
+                | First Slide
+        v-carousel-item
+          v-sheet(color='secondary', height='100%')
+            .d-flex.fill-height.justify-center.align-center
+              .text-h2
+                | Second Slide
+        v-carousel-item
+          v-sheet(color='yellow darken-4', height='100%')
+            .d-flex.fill-height.justify-center.align-center
+              .text-h2
+                | Third Slide
+
   v-divider.my-4
 
   v-container
