181af6b128a32ba2b218e6fd50d50d405825d37e
Atsuhiro <53073064+a2h1r0@users.noreply.github.com>
2023-11-13 21:48:12 +0900

    #1 Vuetifyの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    '@invictus.codes/nuxt-vuetify'
+  ],
+
+  vuetify: {
+    moduleOptions: {
+      treeshaking: true,
+      useIconCDN: true,
+      styles: 'sass',
+      autoImport: true,
+      useVuetifyLabs: true, 
+    }
+  }
 })
